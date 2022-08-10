b569838ce19666d46763edede85d38380c5758b7
khadija <khadija@DESKTOP-P7EB15G>
2022-08-10 10:23:42 +0530

    work commited

---
+++ wish_github/src/wish_github/First.java
@@ -0,0 +1,10 @@
+package wish_github;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ wish_github/src/wish_github/Second.java
@@ -0,0 +1,12 @@
+package wish_github;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20;
+		System.out.println(a+b);
+
+	}
+
+}
